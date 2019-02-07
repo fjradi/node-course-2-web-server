04d6c4c3921f0c9ced3807dff78bedbc4f36f7ff
fjradi <fjradi14@hotmail.com>
2019-02-07 23:26:11 +0700

    Setup start script and heroku port

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname+'/views/partials');
 hbs.registerHelper('getYear', () => {
     return new Date().getFullYear()
 })
-app.use(express.static(__dirname+'/public'));
 app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
@@ -20,20 +20,24 @@ app.use((req, res, next) => {
     })
     next();
 })
+// app.use((req, res, next) => {
+//     res.render('maintenance');
+// })
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.render('home.hbs', {
-        text: 'Welcome to my website',
-        year: new Date().getFullYear()
+    res.render('home', {
+        pageTitle: 'HOME PAGE',
+        welcomeMessage: 'Welcome to my website'
     })
 });
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs', {
-        year: new Date().getFullYear()
+    res.render('about', {
+        pageTitle: 'ABOUT PAGE'
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server up on port 3000');
+app.listen(port, () => {
+    console.log(`Server up on port ${port}`);
 });
