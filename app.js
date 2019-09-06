4d3fafa3346f52d2c290e84de23bfc37b83958cc
fjradi <fjradi14@hotmail.com>
2019-09-06 21:15:37 +0700

    add many comments

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const hbs = require('hbs');
+const express = require('express'); //web server framework module
+const hbs = require('hbs'); //view engine module
 const fs = require('fs');
-var app = express();
+var app = express(); //creating express framework
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname+'/views/partials');
-hbs.registerHelper('getYear', () => {
+app.set('view engine', 'hbs'); //making hbs as view engine
+hbs.registerPartials(__dirname+'/views/partials'); //adding partial files like header or footer
+hbs.registerHelper('getYear', () => { //adding function which is can be accessed in views
     return new Date().getFullYear()
-})
-app.use((req, res, next) => {
+});
+app.use((req, res, next) => { //logger middleware
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
     console.log(log);
@@ -17,33 +17,34 @@ app.use((req, res, next) => {
         if (err){
             console.log('Unable to append file');
         }
-    })
-    next();
-})
-// app.use((req, res, next) => {
-//     res.render('maintenance');
-// })
-app.use(express.static(__dirname + '/public'));
+    });
+    next(); //kalo gk ada ini nanti stop di middleware
+});
+app.use(express.static(__dirname + '/public')); //middleware which register access route in public directory
 
+//home route
 app.get('/', (req, res) => {
-    res.render('home', {
+    res.render('home.hbs', {
         pageTitle: 'HOME PAGE',
         welcomeMessage: 'Welcome to my website'
-    })
+    });
 });
 
+//about route
 app.get('/about', (req, res) => {
     res.render('about', {
         pageTitle: 'ABOUT PAGE'
     });
 });
 
+//projects route
 app.get('/projects', (req, res) => {
     res.render('projects', {
         pageTitle: 'PROJECTS'
     });
 });
 
+//starting server
 app.listen(port, () => {
     console.log(`Server up on port ${port}`);
 });
